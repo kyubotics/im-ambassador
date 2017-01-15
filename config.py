fdcd4d62c2246ce0d0a3a4a4ef7432ed26801ad0
Richard Chien <richardchienthebest@gmail.com>
2017-01-15 19:54:14 +0800

    删除原配置文件

--- config.py
+++
@@ -1,8 +0,0 @@
-config = {
-    'fallback_command': 'natural_language.process',
-    'fallback_command_after_nl_processors': 'ai.tuling123',
-    'command_start_flags': ('/', '／', '来，', '来,'),  # Add '' (empty string) here to allow commands without start flags
-    'command_name_separators': ('->', '::', '/'),  # Regex
-    'command_args_start_flags': ('，', '：', ',', ', ', ':', ': '),  # Regex
-    'command_args_separators': ('，', ','),  # Regex
-}
